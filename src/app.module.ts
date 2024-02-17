522e887168074de00bffb359437ef75ea4b46160
famesensor <sathit.ju@outlook.com>
2024-02-18 01:27:13 +0700

    feat: implement health module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { HealthModule } from './health/health.module'
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             envFilePath: '.env',
         }),
+        HealthModule,
     ],
-    controllers: [AppController],
-    providers: [AppService],
 })
 export class AppModule {}
