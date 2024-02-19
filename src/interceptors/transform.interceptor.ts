3740dbac18d7bdb6b521a5863dd2527ed3d97eef
famesensor <sathit.ju@outlook.com>
2024-02-19 23:35:43 +0700

    refactor: review

--- src/interceptors/transform.interceptor.ts
+++ src/interceptors/transform.interceptor.ts
@@ -1,16 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common'
+import {
+    CallHandler,
+    ExecutionContext,
+    HttpStatus,
+    Injectable,
+    NestInterceptor,
+} from '@nestjs/common'
 import { Request } from 'express'
 import { Observable, map } from 'rxjs'
 
 @Injectable()
 export class TransformInterceptor implements NestInterceptor {
-    intercept(_context: ExecutionContext, next: CallHandler): Observable<any> {
-        const ctx = _context.switchToHttp()
-        const request = ctx.getRequest<Request>()
-
-        let status = 200
-        if (request.method === 'POST') status = 201
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        const request = context.switchToHttp().getRequest<Request>()
+        const status = request.method === 'POST' ? HttpStatus.CREATED : HttpStatus.OK
 
         return next.handle().pipe(map((data) => ({ code: status, message: 'success', data })))
     }
