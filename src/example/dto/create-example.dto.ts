f8cb5acc8c8d8630dd07834aa0b22d26f047acae
famesensor <sathit.ju@outlook.com>
2024-03-07 22:45:25 +0700

    fix: add decorator api property

--- src/example/dto/create-example.dto.ts
+++ src/example/dto/create-example.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsNotEmpty, IsString } from 'class-validator'
 
 export class CreateExampleDto {
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     name!: string
