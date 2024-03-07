186c5789988fabc5ca5422aa9c3f7660f2e5b360
famesensor <sathit.ju@outlook.com>
2024-03-07 22:40:03 +0700

    chore: update schema

--- src/example/dto/create-example.dto.ts
+++ src/example/dto/create-example.dto.ts
@@ -3,9 +3,5 @@ import { IsNotEmpty, IsString } from 'class-validator'
 export class CreateExampleDto {
     @IsNotEmpty()
     @IsString()
-    name?: string
-
-    @IsNotEmpty()
-    @IsString()
-    desc?: string
+    name!: string
 }
--- src/example/example.controller.ts
+++ src/example/example.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common'
+import { ExampleEntity } from 'src/example/entities/example.entity'
+import { ExampleService } from 'src/example/example.service'
 import { CreateExampleDto } from './dto/create-example.dto'
 
 @Controller('example')
 export class ExampleController {
-    constructor() {}
+    constructor(private exampleService: ExampleService) {}
 
     @Post()
-    create(@Body() createExampleDto: CreateExampleDto): void {
-        console.log('body -> ', createExampleDto)
+    async create(@Body() body: CreateExampleDto): Promise<ExampleEntity> {
+        return await this.exampleService.createExample(body)
     }
 }
