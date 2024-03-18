9c5c51f966d117af46f487eba36ed70f8168338a
nutthanon <nutthanon.tho@gmail.com>
2024-03-18 22:14:07 +0700

    feat: find many user / find user by id

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config'
 import { PrismaModule } from 'src/prisma/prisma.module'
 import { ExampleModule } from './example/example.module'
 import { HealthModule } from './health/health.module'
+import { UserModule } from './user/user.module'
 
 @Module({
     imports: [
@@ -12,6 +13,7 @@ import { HealthModule } from './health/health.module'
         HealthModule,
         ExampleModule,
         PrismaModule,
+        UserModule,
     ],
 })
 export class AppModule {}
---
+++ src/user/user.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+import { PrismaModule } from 'src/prisma/prisma.module'
+import { UserController } from './user.controller'
+import { UserRepository } from './user.repository'
+import { UserService } from './user.service'
+
+@Module({
+    imports: [PrismaModule],
+    controllers: [UserController],
+    providers: [UserService, UserRepository],
+})
+export class UserModule {}
