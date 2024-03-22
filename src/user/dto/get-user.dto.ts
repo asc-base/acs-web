6360f52d383ef76c51d3b42990551d7932e05a15
nutthanon <nutthanon.tho@gmail.com>
2024-03-22 19:07:40 +0700

    del: change file name

--- src/user/dto/get-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty, IsNumberString } from 'class-validator'
-
-export class ParamUserDto {
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumberString()
-    id!: string
-}
