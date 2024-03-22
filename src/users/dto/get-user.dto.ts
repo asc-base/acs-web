4d0b18f70a70bb0fc46873c8c2d4e8c5bf52da22
nutthanon <nutthanon.tho@gmail.com>
2024-03-22 19:09:22 +0700

    feat: get-user dto

---
+++ src/users/dto/get-user.dto.ts
@@ -0,0 +1,86 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator'
+
+export class ParamUserDto {
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
+    id!: number
+}
+
+export class QueryUserDto {
+    @ApiProperty({
+        required: false,
+    })
+    @IsString()
+    @IsOptional()
+    username?: string
+
+    @ApiProperty({
+        required: false,
+    })
+    @IsString()
+    @IsOptional()
+    firstnameTh?: string
+
+    @ApiProperty({
+        required: false,
+    })
+    @IsString()
+    @IsOptional()
+    lastnameTh!: string
+
+    @ApiProperty({
+        required: false,
+    })
+    @IsString()
+    @IsOptional()
+    firstnameEn?: string
+
+    @ApiProperty({
+        required: false,
+    })
+    @IsString()
+    @IsOptional()
+    lastnameEn!: string
+
+    @ApiProperty({
+        required: false,
+    })
+    @IsString()
+    @IsOptional()
+    email!: string
+
+    @ApiProperty({
+        required: false,
+    })
+    @IsString()
+    @IsOptional()
+    phone!: string
+
+    @ApiProperty({
+        required: false,
+    })
+    @IsString()
+    @IsOptional()
+    role!: string
+
+    @ApiProperty({
+        required: false,
+    })
+    @IsNumber()
+    page!: number
+
+    @ApiProperty({
+        required: false,
+    })
+    @IsNumber()
+    pageSize!: number
+}
+
+export interface Pageable<T> {
+    rows: T[]
+    totalRecords: number
+    page: number
+    pageSize: number
+}
