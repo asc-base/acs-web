c089748e26e60de6f89a544f0ff4714283fce6fb
famesensor <sathit.ju@outlook.com>
2024-03-25 22:02:29 +0700

    feat: implement `ValidationException`

---
+++ src/exceptions/validate.exception.ts
@@ -0,0 +1,17 @@
+import { HttpException } from '@nestjs/common'
+import { ValidationError } from 'class-validator'
+import Exception from 'src/interfaces/exception.interface'
+
+const ValidationException = (errors: ValidationError[]): HttpException => {
+    const message = errors.reduce(
+        (p, c) => {
+            p[c?.property] = [...Object.values(c.constraints ?? {})]
+            return p
+        },
+        {} as { [index: string]: string[] },
+    )
+
+    return Exception.createBadRequestException(message)
+}
+
+export default ValidationException
