b079c8c88bcb0fa7f274e2ca2ba0b617f4db5b0e
nutthanon <nutthanon.tho@gmail.com>
2024-03-25 22:33:01 +0700

    feat: add role entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,5 @@
 import { Users } from '@prisma/client'
+import { RoleEntity } from 'src/roles/entities/role.entity'
 import { StudentEntity } from 'src/students/entities/student.entity'
 
 export class UserEntity implements Users {
@@ -10,14 +11,15 @@ export class UserEntity implements Users {
     firstnameEn!: string | null
     lastnameEn!: string | null
     nickname!: string | null
+    roleId!: number
     email!: string
     phone!: string
     password!: string
-    roleId!: number
     createdDate!: Date
     createdBy!: number
     updatedDate!: Date
     updatedBy!: number | null
 
+    Role?: RoleEntity | null
     student?: StudentEntity | null
 }
