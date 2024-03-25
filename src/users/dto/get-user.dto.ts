d278876290e58c30ce51e5e61ee9cde5663d619e
nutthanon <nutthanon.tho@gmail.com>
2024-03-25 12:28:13 +0700

    feat: order field and transform value

--- src/users/dto/get-user.dto.ts
+++ src/users/dto/get-user.dto.ts
@@ -1,11 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator'
+import { Transform } from 'class-transformer'
+import { IsBoolean, IsEnum, IsNumber, IsOptional, IsString } from 'class-validator'
 
-export class ParamUserDto {
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsNumber()
-    id!: number
+export enum ORDER_FIELD {
+    USERNAME = 'username',
+    FIRSTNAME_TH = 'firstnameTh',
+    LASTNAME_TH = 'lastnameTh',
+    FIRSTNAME_EN = 'firstnameEn',
+    LASTNAME_EN = 'lastnameEn',
+    EMAIL = 'email',
+    PHONE = 'phone',
+    ROLE = 'role',
+    CREATED_DATE = 'createdDate',
+    UPDATED_DATE = 'updatedDate',
+}
+
+export enum ORDER_BY {
+    ASC = 'asc',
+    DESC = 'desc',
 }
 
 export class QueryUserDto {
@@ -61,22 +73,46 @@ export class QueryUserDto {
     @ApiProperty({
         required: false,
     })
-    @IsString()
-    @IsOptional()
-    role!: string
+    @IsNumber()
+    @Transform(({ value }) => parseInt(value))
+    Role!: number
 
     @ApiProperty()
     @IsNumber()
+    @Transform(({ value }) => parseInt(value))
     page!: number
 
     @ApiProperty()
     @IsNumber()
+    @Transform(({ value }) => parseInt(value))
     pageSize!: number
-}
 
-export interface Pageable<T> {
-    rows: T[]
-    totalRecords: number
-    page: number
-    pageSize: number
+    @ApiProperty({
+        required: false,
+        enum: ORDER_BY,
+        default: ORDER_BY.ASC,
+    })
+    @IsString()
+    @IsOptional()
+    @IsEnum(ORDER_BY)
+    orderBy: string = ORDER_BY.ASC
+
+    @ApiProperty({
+        required: false,
+        enum: ORDER_FIELD,
+        default: ORDER_FIELD.CREATED_DATE,
+    })
+    @IsString()
+    @IsOptional()
+    @IsEnum(ORDER_FIELD)
+    orderField: ORDER_FIELD = ORDER_FIELD.CREATED_DATE
+
+    @ApiProperty({
+        required: false,
+        default: true,
+    })
+    @IsBoolean()
+    @IsOptional()
+    @Transform(({ value }) => value === 'true')
+    returnStudent!: boolean
 }
