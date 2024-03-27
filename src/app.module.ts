3883d9f0d1435a223a7f759b97da5817b5d00094
famesensor <sathit.ju@outlook.com>
2024-03-27 23:20:46 +0700

    refactor: remove unused

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { PrismaModule } from 'src/providers/databases/prisma/prisma.module'
-import { RepositoriesModule } from 'src/repositories/repositories.module'
 import { ExampleModule } from './modules/example/example.module'
 import { HealthModule } from './modules/health/health.module'
 import { UsersModule } from './modules/users/users.module'
@@ -11,7 +10,6 @@ import { UsersModule } from './modules/users/users.module'
         ConfigModule.forRoot({
             envFilePath: '.env',
         }),
-        RepositoriesModule,
         HealthModule,
         ExampleModule,
         PrismaModule,
